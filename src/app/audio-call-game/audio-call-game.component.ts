dbe560ae606ae8b7f37ab920f6ffc1eadcf4cd34
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-08-28 17:16:32 +0300

    feat: add 3 game status(menu, play, end) in html and styles for menu

--- src/app/audio-call-game/audio-call-game.component.ts
+++ src/app/audio-call-game/audio-call-game.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-audio-call-game',
   templateUrl: './audio-call-game.component.html',
   styleUrls: ['./audio-call-game.component.scss'],
 })
-export class AudioCallGameComponent implements OnInit {
+export class AudioCallGameComponent {
+  public levels: string[] = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 
-  constructor() { }
+  public gameStatus = 'menu';
 
-  ngOnInit(): void {
+  public prevClass: HTMLElement | undefined;
+
+  public isAnswerChosen = false;
+
+  public playGame(): void {
+    this.gameStatus = 'play';
+    this.isAnswerChosen = false;
   }
 
+  public addClass(event: MouseEvent) {
+    this.prevClass?.classList.remove('active');
+    (<HTMLElement>event?.target).classList.add('active');
+    this.prevClass = (<HTMLElement>event.target);
+  }
 }
