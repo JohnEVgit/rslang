9b8ca8f76937937beb85479a59c1d1078ac9b413
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-08-28 15:57:34 +0300

    init: start audio-call-game

---
+++ src/app/audio-call-game/audio-call-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-call-game',
+  templateUrl: './audio-call-game.component.html',
+  styleUrls: ['./audio-call-game.component.scss'],
+})
+export class AudioCallGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
