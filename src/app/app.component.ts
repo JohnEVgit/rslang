dae6cb43234dfe4f2c2338d7c2baf7fb7278155a
John <john13ev@gmail.com>
2022-08-28 18:51:27 +0400

    feat: add textbook page base markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rslang';
-
-  isOpenMenu = false;
-
-  openMenu(isOpen: boolean) {
-    this.isOpenMenu = isOpen;
-  }
 }
--- src/app/textbook/textbook.component.ts
+++ src/app/textbook/textbook.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Word {
+  'id': 'string',
+  'group': 0,
+  'page': 0,
+  'word': 'string',
+  'image': 'string',
+  'audio': 'string',
+  'audioMeaning': 'string',
+  'audioExample': 'string',
+  'textMeaning': 'string',
+  'textExample': 'string',
+  'transcription': 'string',
+  'wordTranslate': 'string',
+  'textMeaningTranslate': 'string',
+  'textExampleTranslate': 'string'
+}
 
 @Component({
   selector: 'app-textbook',
   templateUrl: './textbook.component.html',
-  styleUrls: ['./textbook.component.scss']
+  styleUrls: ['./textbook.component.scss'],
 })
 export class TextbookComponent implements OnInit {
+  words: Word[] = [];
 
-  constructor() { }
+  backendUrl = 'https://angular-learnwords.herokuapp.com/';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<Word[]>(`${this.backendUrl}words`).subscribe((response) => {
+      console.log('Response', response);
+      this.words = response;
+    });
   }
-
 }
