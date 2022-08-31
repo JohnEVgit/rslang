56290e0ecfa2272362e9b3608a45250a83b1a14b
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-08-31 20:17:38 +0300

    feat: add check answer method, timer for game

--- src/app/sprint-game/sprint-game.component.ts
+++ src/app/sprint-game/sprint-game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 import { SprintGameService } from '../services/sprint-game.service';
 import { Word } from '../data/interfaces';
 
 @Component({
   selector: 'app-sprint-game',
   templateUrl: './sprint-game.component.html',
-  styleUrls: ['./sprint-game.component.scss']
+  styleUrls: ['./sprint-game.component.scss'],
 })
 export class SprintGameComponent {
   public levels: string[] = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
@@ -26,6 +27,8 @@ export class SprintGameComponent {
 
   public wordTranslate = '';
 
+  public time = 30;
+
   constructor(private sprintGameService: SprintGameService) {}
 
   public chooseLevel(group: number) {
@@ -34,13 +37,13 @@ export class SprintGameComponent {
         words.forEach((word, index) => {
           this.randomWords.push(word);
         });
-        this.getWord();
+        this.generateQuestion();
         this.isStartDisabled = false;
       });
     console.log(this.randomWords);
   }
 
-  private getWord(): void {
+  private generateQuestion(): void {
     this.word = this.randomWords[this.wordIndex].word;
     this.wordTranslate = this.randomWords[this.getRandomTranslateIndex()].wordTranslate;
   }
@@ -65,13 +68,34 @@ export class SprintGameComponent {
 
   public playGame(): void {
     this.gameStatus = 'play';
+    timer(1000, 1000).subscribe(() => {
+      this.time -= 1;
+      if (this.time === 0) {
+        this.gameStatus = 'end';
+      }
+    });
   }
 
   public nextQuestion(): void {
-    this.getWord();
+    this.generateQuestion();
+  }
+
+  public menuGame(): void {
+    this.gameStatus = 'menu';
+    this.randomWords = [];
+    this.wordIndex = 0;
+    this.time = 30;
+  }
+
+  public checkMouseAnswer(isRight: boolean) {
+    if (isRight && this.randomWords[this.wordIndex].wordTranslate === this.wordTranslate) {
+      console.log(true);
+    } else if (!isRight && this.randomWords[this.wordIndex].wordTranslate !== this.wordTranslate) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
     this.wordIndex += 1;
-    console.log(this.word);
-    console.log(this.wordTranslate);
-    console.log(this.wordIndex)
+    this.nextQuestion();
   }
 }
